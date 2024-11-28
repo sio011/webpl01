2529d168f995af87f4b57ba8997237bedd66f52b
unknown <Kiippei@icloud.com>
2024-11-28 16:34:55 +0900

    WEBサイト初期

---
+++ script.js
@@ -0,0 +1,9 @@
+        // ハンバーガーメニュー外をクリックしたときにメニューを閉じる
+        window.addEventListener('click', function(e) {
+            var humMenu = document.getElementById('humMenu');
+            var humCheck = document.getElementById('humCheck');
+            // メニュー以外をクリックした場合にメニューを閉じる
+            if (!humMenu.contains(e.target)) {
+                humCheck.checked = false;
+            }
+        });   
